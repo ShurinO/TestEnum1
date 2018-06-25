465955087304d385436f59b3e190834a46f8d57d
ShurinO <arestarx@ukr.net>
2018-06-25 09:31:34 +0300

    Initial commit

---
+++ src/com/brainacad/oop/testenum1/Main.java
@@ -0,0 +1,9 @@
+package com.brainacad.oop.testenum1;
+
+public class Main {
+    public static void main(String[] args) {
+        for (MyDayOfWeek myDayOfWeek:MyDayOfWeek.values()) {
+            System.out.println(myDayOfWeek);
+        }
+    }
+}
---
+++ src/com/brainacad/oop/testenum1/MyDayOfWeek.java
@@ -0,0 +1,5 @@
+package com.brainacad.oop.testenum1;
+
+public enum MyDayOfWeek {
+    SUNDAY, MONDAY, TUESDAY, WEDNESDAY, THURSDAY, FRIDAY, SATURDAY
+}
